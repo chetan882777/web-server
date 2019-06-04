1789be72cd072dd958561d619cb49c9b492a672d
chetan882777 <chetan882777@gmail.com>
2019-06-04 21:45:13 +0530

    using hbs for dynamic pages

--- src/app.js
+++ src/app.js
@@ -4,7 +4,27 @@ const path = require("path")
 const app = express()
 const publicDirPath = path.join(__dirname , '../public')
 
+app.set("view engine" , "hbs")
 app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather app'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: "For more help contact xxxxx"
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About developer',
+        name: 'Chetan Pawar'
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
