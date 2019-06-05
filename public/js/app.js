2f40ebbfa85d3959b60705d08318e091b8c29f8a
chetan882777 <chetan882777@gmail.com>
2019-06-05 12:31:07 +0530

    Browser Http request with fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 console.log("This is client side js file!")
+
+fetch("http://localhost:3000/weather?address=barwani").then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data)
+        }
+    })
+})
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -6,7 +6,7 @@ const geoCode = function(address, callback){
     const geoUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/ "+ address +".json?access_token=pk.eyJ1IjoiY2hldGFucGF3YXIiLCJhIjoiY2p3Z3R0MDhoMGNxdjQwbGJnajM2ZHo4byJ9.5azLiuRBvbXvy3xE0tDovw&limit=1"
 
 
-    request({ url: geoUrl, json: true }, (error, {body}) => {
+    request({ url: geoUrl, json: true }, (error, {body} = {}) => {
     
         if (error) {
             callback('Unable to connect to location services!', undefined)
