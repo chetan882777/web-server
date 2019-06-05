583aeba041af681c22f45b8a8e46a4af7270c2f4
chetan882777 <chetan882777@gmail.com>
2019-06-05 13:08:28 +0530

    Wiring up user interface

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,29 @@
 console.log("This is client side js file!")
 
-fetch("http://localhost:3000/weather?address=barwani").then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            console.log(data.error)
-        }else{
-            console.log(data)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    message1.textContent = "Loading..."
+    message2.textContent = ''
+
+    fetch("http://localhost:3000/weather?address="+ location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+                message1.textContent = data.error
+            } else {
+                console.log(data)
+                message1.textContent = data.location
+                message2.textContent = data.data
+            }
+        })
     })
+
 })
